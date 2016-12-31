73d46102e44f79903637cbb66dbe4ad0cedf63d7
Chaow Porkaew <pchaowmobile@gmail.com>
2016-12-31 08:46:34 +0700

    install laratrust and soap

--- config/app.php
+++ config/app.php
@@ -178,6 +178,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Artisaninweb\SoapWrapper\ServiceProvider::class,
+        Laratrust\LaratrustServiceProvider::class,
+
+
     ],
 
     /*
@@ -225,6 +229,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
+        'SoapWrapper' => 'Artisaninweb\SoapWrapper\Facades\SoapWrapper',
+
 
     ],
 
