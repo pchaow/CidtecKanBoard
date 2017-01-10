9f04626ebaba815dc2550f4f69076c1c1a224f7f
Natthawut Jantapoon <dear.natthawut@gmail.com>
2017-01-10 15:08:16 +0700

    update migration
    - boards, board_user, lanes, cards, card_user

---
+++ database/migrations/2017_01_10_054943_create_lanes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('lanes', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name')->nullable();
+          $table->timestamps();
+          $table->integer('boards_id')->unsigned();
+
+          $table->foreign('boards_id')->references('id')->on('boards')
+              ->onUpdate('cascade')->onDelete('cascade');
+      });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+          Schema::drop('lanes');
+    }
+}
