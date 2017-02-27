09d3f47c8fbf2e41175bf16762c77a754ff41dfd
Chaow Porkaew <pchaowmobile@gmail.com>
2017-02-27 21:15:57 +0700

    fix model

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -7,23 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class Card extends Model
 {
     protected $fillable = ['name', 'description', 'user_id', 'lanes_id', 'startdate', 'duedate', 'checklists'];
+
     //get  card owner
     public function ownerCard()
-   {
-       return $this->belongsTo('App\Models\User', 'user_id');
-   }
+    {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
 
-   //get Lane of card
-   public function lane()
-  {
-      return $this->belongsTo('App\Models\Lane', 'lanes_id');
-  }
+    //get Lane of card
+    public function lane()
+    {
+        return $this->belongsTo('App\Models\Lane', 'lanes_id');
+    }
 
-  //get all member in card
-  public function mamebersCard()
- {
-     return $this->belongsToMany('App\Models\User','card_user','cards_id','users_id');
- }
+    //get all member in card
+    public function mamebersCard()
+    {
+        return $this->belongsToMany('App\Models\User', 'card_user', 'cards_id', 'users_id');
+    }
 
+    protected $casts = [
+        'checklists' => 'array',
+    ];
 
 }
