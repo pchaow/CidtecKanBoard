730e7deed6d6c9b1777bfb90a53f6d5fa6479963
Chaow Porkaew <pchaowmobile@gmail.com>
2017-05-11 15:35:23 +0700

    update scss remove border bottom on userheader

--- resources/views/user/userboardmenu.blade.php
+++ resources/views/user/userboardmenu.blade.php
@@ -1,10 +1,16 @@
 <div class="container header">
     <ul class="nav nav-tabs">
-        <li role="presentation"
-            class="{{ Request::is($board->user->username.'/'.$board->name) ? 'active' : '' }}"
-        ><a href="/{{$board->user->username}}/{{$board->name}}">กระดาน</a></li>
-        <li role="presentation"
-            class="{{ Request::is($board->user->username.'/'.$board->name.'/edit') ? 'active' : '' }}"
-        ><a href="/{{$board->user->username}}/{{$board->name}}/edit">แก้ไขกระดาน</a></li>
+        <li class="{{ Request::is($board->user->username.'/'.$board->name) ? 'active' : '' }}">
+            <a href="/{{$board->user->username}}/{{$board->name}}">กระดาน</a>
+        </li>
+        <li class="{{ Request::is($board->user->username.'/'.$board->name.'/edit') ? 'active' : '' }}">
+            <a href="/{{$board->user->username}}/{{$board->name}}/edit">แก้ไขกระดาน</a>
+        </li>
+        <li class="{{ Request::is($board->user->username.'/'.$board->name.'/lanes') ? 'active' : '' }}">
+            <a href="/{{$board->user->username}}/{{$board->name}}/lanes">ปรับกระดาน</a>
+        </li>
+        <li class="{{ Request::is($board->user->username.'/'.$board->name.'/members') ? 'active' : '' }}">
+            <a href="/{{$board->user->username}}/{{$board->name}}/members">สมาชิก</a>
+        </li>
     </ul>
 </div>
